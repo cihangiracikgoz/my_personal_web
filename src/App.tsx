5aadb5bc201dbc3bd264146fab51befc0dd54469
Cihangir <cihangiracikgoz01@gmail.com>
2026-04-02 14:32:49 +0100

    feature: Add Projects section with project cards layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Journey from "./components/Journey/Journey";
+import Projects from "./components/Projects/Projects";
 
 export default function App() {
-
   return (
     <>
       <Header></Header>
       <About></About>
       <Journey></Journey>
+      <Projects></Projects>
     </>
   )
 }
